89d7a1a5f1876daa2127947d8adbbb9612af5e10
elkuus <eliskuusik@gmail.com>
2019-02-07 13:09:33 +0200

    Update index.js

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,7 +1,11 @@
-
-new Vue({
+var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
